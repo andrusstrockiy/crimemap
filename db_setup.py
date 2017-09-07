e05827dfe5a1bdc6e172da15899c2bd01bb43036
Andruss Trockiy <trockiy@gmail.com>
2017-09-08 00:37:45 +0300

    database setup script

---
+++ db_setup.py
@@ -0,0 +1,26 @@
+import pymysql
+import dbconfig
+
+
+connection = pymysql.connect(host='localhost',
+                             user=dbconfig.db_user,
+                             passwd=dbconfig.db_password)
+
+try:
+    with connection.cursor() as cursor:
+        sql = 'CREATE DATABASE IF NOT EXISTS crimemap default charset utf8'
+        cursor.execute(sql)
+        sql = 'create table if not exists crimemap.crimes(' \
+              'id int not null auto_increment,' \
+              'latitude float(10,6),' \
+              'longtitude float(10,6),' \
+              'date datetime,' \
+              'category varchar(50),' \
+              'description varchar(100),' \
+              'updated_at timestamp,' \
+              'PRIMARY KEY (id)' \
+              ')'
+        cursor.execute(sql)
+        connection.commit()
+finally:
+    connection.close()
